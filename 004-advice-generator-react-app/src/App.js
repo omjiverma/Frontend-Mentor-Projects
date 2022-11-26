bf241a96ff9691a3b3bd0014b7f41cf495b6362a
omjiverma <omjidvlpr@gmail.com>
2022-11-26 19:56:17 +0530

    advice-generator a
    .

--- 004-advice-generator-react-app/src/App.js
+++ 004-advice-generator-react-app/src/App.js
@@ -1,16 +1,36 @@
 import desktopDivider from './pattern-divider-desktop.svg'
 import dice from './icon-dice.svg'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [advice, setAdvice] = useState([])
+
+
+  const getAdvice = () =>{
+    console.log("Button Clicked")
+    axios.get("https://api.adviceslip.com/advice")
+    .then((response) => {
+      setAdvice(response.data.slip)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="app">
       <div className="card">
-        <h1 className="card-header">Advice #117</h1>
+        <h1 className="card-header">Advice #{advice.id}</h1>
         <p className="card-text">
-        You spend half your life asleep or in bed. It's worth spending money on a good mattress, decent pillows and a comfy duvet."
+          {advice.advice}
         </p>
         <img src={desktopDivider} alt="desktop-divider" className="desktop-divider" />
-        <button className="card-button"><img src={dice} alt="dice" /></button>
+        <button className="card-button" onClick = {getAdvice} ><img src={dice} alt="dice" /></button>
       </div>
     </div>
   );
